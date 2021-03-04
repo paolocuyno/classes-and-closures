f1432300c051517c7d641fe96b8bccbd35d7b50a
Paolo Cuyno <paolo.cuyno@gmail.com>
2021-03-03 23:07:51 -0700

    lab1

--- closures.js
+++ closures.js
@@ -23,14 +23,14 @@ function outer() {
 */
   
 // Code Here
-
+let inner=outer()
 
 
 //Once you do that, invoke inner.
 
 //Code Here
 
-
+inner()
 
 ////////// PROBLEM 2 //////////
 
@@ -52,6 +52,9 @@ function callFriend(name) {
 */
 
 //Code Here
+let callJake=callFriend('Jake','435-555-9248')
+callJake()
+
 
 
 
@@ -62,15 +65,23 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter(){
+  let count=0;
+  function addOne(){
+  return ++count;
+  }
+  return addOne
+}
+
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +98,23 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  let counter=value;
   return {
+    inc:function inc(){
+      return ++ value
+    },
+    dec:function dec(){
+      return -- value
+    }
 
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +130,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+  function message(){
+    return "You're doing awesome, keep it up"+" "+firstname+" "+lastname+"."
+  }
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +163,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+  publicMethod: function(){
+    return privateMethod()
+  }  
   };
 })();
 
@@ -158,12 +180,20 @@ var module = (function() {
   takeAwayFromSecret should have a parameter that takes away from the secret number returning the updated secret number.
 */
 
+
 function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
-  };
+   addToSecret:function (num){
+     return secret+=num;
+   }
+  ,
+  takeAwayFromSecret:function (num){
+    return secret-=num;
+  }
+
+  }
 }
 
 
